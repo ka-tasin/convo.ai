13375b8f7f17d6ce1bedca5c215b57ce9a5e2626
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-10-02 15:28:04 +0600

    frontend model created

---
+++ server/src/models/chat.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMessage {
+  sender: string;
+  receiver: string;
+  content: string;
+  createdAt: Date;
+}
+
+export interface IChat extends Document {
+  participants: string[]; // array of user IDs
+  messages: IMessage[];
+}
+
+const MessageSchema: Schema = new Schema({
+  sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const ChatSchema: Schema = new Schema({
+  participants: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+  messages: [MessageSchema],
+});
+
+export default mongoose.model<IChat>("Chat", ChatSchema);
