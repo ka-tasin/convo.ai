d3e65c923bc8016f11e91c0aad8b9acd2a71bd52
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-10-16 14:59:41 +0600

    backend baseurl has been made live on frontend

--- client/src/utils/apiClient.tsx
+++ client/src/utils/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://convo-ai-hdf2.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
